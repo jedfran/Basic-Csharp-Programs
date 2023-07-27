6fa0be77b98c8f5e2e141c1292c7a2972bba6de5
jedfran <jedofrancisco12@gmail.com>
2023-07-27 12:33:04 -0500

    Practiced Exception Handling using Try and Catch and Finally.

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //try/catch block is used when error happens throughout the code
+        try
+            {
+            Console.WriteLine("Enter a number: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number: ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dividing the two numbers...");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals: " + numberThree);
+            Console.ReadLine();
+
+            }
+        catch (FormatException ex)
+            {
+            //Message when error appears
+            Console.WriteLine("Please type a whole number.");
+            return;
+            }
+        catch (DivideByZeroException ex)
+            {
+            
+            Console.WriteLine("Please don't divide by zero.");
+            }
+        //General Exception
+        catch (Exception ex)
+            {
+            Console.WriteLine(ex.Message);
+            }
+        //The finally will run no matter what, if the catch 'catches' something it will run,
+        // if it doesn't catch anything it will still run.
+        finally
+            {
+            Console.ReadLine();
+            }
+        Console.ReadLine();
+        
+        }
+    }
---
+++ Iteration Assignment/Iteration Assignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Iteration_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////PART 1
+            ////An string array of animals
+            //string[] animals = { "Dog", "Cat", "Bear", "Lion", "Fish" };
+
+            ////Asking user to type "I Like"
+            //Console.WriteLine("Please Enter \"I like\" ");
+            //string userInput = Console.ReadLine();
+
+            ////For loop to update the array
+            //for (int i = 0; i < animals.Length; i++)
+            //{
+            //    animals[i] = userInput + " " + animals[i];
+            //}
+            ////For loop to print each of the string
+            //for (int i = 0; i < animals.Length; i++)
+            //{
+            //    Console.WriteLine(animals[i]);
+            //}
+
+            //Console.ReadLine();
+
+            //PART 2
+            //Creating a infinite loop
+            int count = 0;
+            while(true)
+            {
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
