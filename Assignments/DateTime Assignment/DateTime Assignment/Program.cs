c7e206c3c17c2aeb10eb5d452a2a2424cfda2f29
jedfran <jedofrancisco12@gmail.com>
2023-08-09 12:42:11 -0500

    Using DateTime

---
+++ Assignments/DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints Current Date Time 
+            Console.WriteLine(DateTime.Now);
+            //Ask for user to enter a number
+            Console.WriteLine("Please Enter a number: ");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            //Prints to the console the exact time it will be in X hours
+            Console.Write("{0} + {1} hours will be {2}", DateTime.Now, hour, DateTime.Now.AddHours(hour));
+            Console.ReadLine();
+
+        }
+    }
+}
